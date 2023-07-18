7aa9a0d7841fe5409736d40f3078af50ba6f41e7
Your Name <irfana.27@gmail.com>
2023-07-18 12:28:43 -0700

    initial commit

---
+++ book/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('about/', views.about,name='about'),
+    path('signup/,', views.signup,name='signup'),
+    path('book/<int:book_id>/',views.detail,name='detail'),
+    path('<int:book_id>/create/',views.createreview,name='createreview'),
+    path('review/<int:review_id>/',views.updatereview,name='updatereview'),
+    path('review/<int:review_id>/delete/',views.deletereview,name='deletereview'),
+]
---
+++ book/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from .models import Book,Review
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+def home(request):
+    searchTerm = request.GET.get('searchBook')
+    if searchTerm:
+        books = Book.objects.filter(title__icontains=searchTerm)
+        if books.count() ==0:
+              books = Book.objects.all()
+        
+    else:
+        books = Book.objects.all()
+    
+    context = {
+        'searchTerm': searchTerm,
+        'books': books
+    }
+    return render(request, 'home.html',context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html',{'email':email})
+
+def detail(request,book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    reviews = Review.objects.filter(book = book)
+    return render(request, 'detail.html', {'book':book,'reviews':reviews})
+@login_required
+def createreview(request,book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'GET':
+        form = ReviewForm()
+        return render(request, 'createreview.html', {'book':book,'form':form})
+    else:
+        try:
+            form = ReviewForm(request.POST)
+            newReview = form.save(commit=False)
+            newReview.user = request.user
+            newReview.book = book
+            newReview.save()
+            return redirect('detail', newReview.book.id)
+        except ValueError:
+            return render(request, 'createreview.html', {'book':book,'form':form, 'error':'bad data passed in'})
+        
+@login_required
+def updatereview(request,review_id):
+    review = get_object_or_404(Review,pk = review_id, user = request.user)
+    if request.method == 'GET':
+        form = ReviewForm(instance = review)
+        return render(request, 'updatereview.html', {'review':review,'form':form})
+    else:
+        try:
+            form = ReviewForm(request.POST, instance = review)
+            form.save()
+            return redirect('detail', review.book.id)
+        except ValueError:
+            return render(request, 'updatereview.html', {'review':review,'form':form, 'error':'bad data passed in'})
+        
+@login_required
+def deletereview(request,review_id):
+    review = get_object_or_404(Review,pk = review_id, user = request.user)
+    review.delete()
+    return redirect('detail', review.book.id)
